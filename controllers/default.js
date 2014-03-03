692a353ef5d76a003226d674b4a3ad23eab217b8
kOssi <kossi@kossis-MacBook.local>
2014-03-03 16:01:36 +0100

    first configs

--- controllers/default.js
+++ controllers/default.js
@@ -1,9 +1,14 @@
 exports.install = function(framework) {
 	framework.route('/', view_index);
+	framework.route('/japan', view_japan);
 };
 
 function view_index() {
 	var self = this;
-	// var output = "Nachricht";
 	self.view('index');
+}
+
+function view_japan() {
+	var self = this;
+	self.view('japan');
 }
